0a154fcbab6e4c965fec24075230c120df7abd1f
ilyamore88 <themorozik@gmail.com>
2018-11-12 16:11:40 +0300

    done

---
+++ ПР 15/prakt15/StudentsSTL/Group.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "Group.h"
+#include "Student.h"
+
+
+using namespace std;
+
+
+Group::Group(string name)
+{
+	this->name = name;
+}
+Group::Group()
+{
+	name = "Не определена";
+}
+void Group::setName(string newName)
+{
+	name = newName;
+}
+
+string Group::getName()
+{
+	return name;
+}
+
+int Group::getSize() {
+	return masSt.size();
+}
+
+void Group::addStudent(Student* newStudent) {
+	masSt.insert(newStudent);
+}
+
+void Group::delStudent(Student* oldStudent) {
+	masSt.erase(oldStudent);
+}
+
+void Group::GroupSort() {
+}
+
+void Group::GroupOut() {
+	iter = masSt.begin();
+	while (iter != masSt.end()) {
+		(*iter++)->display();
+	}
+}
+
+Student* Group::findStudent(string searchName, string searchLastName) {
+	Student *temp = new Student(searchName, searchLastName);
+	iter = masSt.lower_bound(temp);
+	delete temp;
+	return (*iter);
+}
+
+
+
+
+
+
+
---
+++ ПР 15/prakt15/StudentsSTL/Group.h
@@ -0,0 +1,29 @@
+#pragma once /* Защита от двойного подключения заголовочного файла */
+#include <string>
+#include "Student.h"
+#include <list>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+class Group
+{
+private:
+	string name;
+	multiset<Student*, compareStudent> masSt;
+	multiset<Student*, compareStudent>::iterator iter;
+
+public:
+	Group(string name);
+	Group();
+
+	void setName(string newName);
+	string getName();
+	int getSize();
+	void addStudent(Student* newStudent);
+	void delStudent(Student* oldStudent);
+	Student* findStudent(string, string);
+	void GroupSort();
+	void GroupOut();
+};
